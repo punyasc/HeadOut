d3c92a7f812bcc682df573309c78aeaa363a59a1
Punya Chatterjee <punya@stanford.edu>
2018-02-15 19:47:46 -0800

    Initial Commit

---
+++ TimeToLeave/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeToLeave
+//
+//  Created by Punya Chatterjee on 2/15/18.
+//  Copyright © 2018 Punya Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
